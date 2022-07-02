5aa7171177770a541ec24a45688108465a738d50
abr-ya <gamer05@list.ru>
2022-07-02 14:29:48 +0300

    feat: Add Order Method

--- assets/scripts/components/Cart.js
+++ assets/scripts/components/Cart.js
@@ -12,6 +12,15 @@ class Cart extends Component {
     return this.items.reduce((acc, item) => acc + item.price, 0)
   }
 
+  constructor(renderHookId) {
+    super(renderHookId, false); // не рендерим, пока нет обработчика
+    this.orderNow = () => {
+      console.log('Ordering...');
+      console.log(this.items);
+    }
+    this.render();
+  }
+
   addProduct(product) {
     const updatedItems = [...this.items, product];
     this.cartItems = updatedItems;
@@ -24,6 +33,8 @@ class Cart extends Component {
       <h2>Total: \$${0}</h2>
       <button>Order Now!</button>
     `;
+    const orderButton = cartEl.querySelector('button');
+    orderButton.addEventListener('click', this.orderNow)
     this.totalEl = cartEl.querySelector('h2');
 
     return cartEl;
